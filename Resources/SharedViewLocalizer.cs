40996ee4ef2d2c7220b0255254555b7710d9553a
howardhee <heecheeho@hotmail.com>
2022-08-10 11:23:41 +0800

    init

---
+++ Resources/SharedViewLocalizer.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Localization;
+using System.Reflection;
+
+namespace Multi7.Resources
+{
+    public class SharedViewLocalizer : ISharedViewLocalizer
+    {
+        private readonly IStringLocalizer localizer;
+
+        public SharedViewLocalizer(IStringLocalizerFactory factory)
+        {
+            var type = typeof(SharedResource);
+            var assemblyName = new AssemblyName(type.GetTypeInfo().Assembly.FullName);
+            this.localizer = factory.Create("SharedResource", assemblyName.Name);
+        }
+
+        public LocalizedString this[string key] => this.localizer[key];
+
+        public LocalizedString GetLocalizedString(string key)
+        {
+            return this.localizer[key];
+        }
+    }
+
+
+
+    public interface ISharedViewLocalizer
+    {
+        public LocalizedString this[string key]
+        {
+            get;
+        }
+
+        LocalizedString GetLocalizedString(string key);
+    }
+
+
+    public class SharedResource
+    {
+    }
+}
